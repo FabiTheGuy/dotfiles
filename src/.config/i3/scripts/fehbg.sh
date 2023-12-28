78287974ab31d25ba78cf4d21acf3b4f52c23def
FabiTheGuy <fabian.doeller@mailfence.com>
2023-12-28 09:43:55 +0100

    add: i3-configuration/feh-configuration

---
+++ src/.config/i3/scripts/fehbg.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+feh --no-fehbg --bg-scale '/home/$USER/Pictures/wallpapers/mon-1-5760x3240.jpg' '/home/$USER/Pictures/wallpapers/mon-2-3840x2160.jpg' '/home/$USER/Pictures/wallpapers/mon-3-2160x3840.jpg' 
